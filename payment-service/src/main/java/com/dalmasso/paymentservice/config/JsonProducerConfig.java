65a41dfc4d73754c17333b7985a26499ffb2f172
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2024-01-02 19:49:52 -0300

    Add producer config

---
+++ payment-service/src/main/java/com/dalmasso/paymentservice/config/JsonProducerConfig.java
@@ -0,0 +1,38 @@
+package com.dalmasso.paymentservice.config;
+
+import lombok.AllArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+@Configuration
+@AllArgsConstructor
+public class JsonProducerConfig {
+
+    private final KafkaProperties properties;
+
+    @Bean
+    public ProducerFactory producerFactory() {
+        var configs = new HashMap<String, Object>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory(configs, new StringSerializer(), new JsonSerializer());
+    }
+
+    public KafkaTemplate<String, Serializable> jsonKafkaTemplate(ProducerFactory producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
--- payment-service/src/main/java/com/dalmasso/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/dalmasso/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,30 @@ package com.dalmasso.paymentservice.service.impl;
 
 import com.dalmasso.paymentservice.model.Payment;
 import com.dalmasso.paymentservice.service.PaymentService;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Log4j2
 @Service
+@AllArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
     @Override
+    @SneakyThrows
     public void sendPayment(Payment payment) {
         log.info("PAYMENT_SERVICE_IMPL ::: Recebi pagamento {}", payment);
+
+        // PARA SIMULAR UM PROCESSAMENT
+        Thread.sleep(1000);
+
+        log.info("Enviando o pagamento");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
